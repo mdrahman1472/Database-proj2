757cd8168505e8371751ece441a2a7dfd7bb2123
mdrahman1472 <mdrahman1472@yahoo.com>
2016-12-28 00:54:32 -0500

    deletion of bid = 0 is done

--- data_proj_2.py
+++ data_proj_2.py
@@ -1,4 +1,5 @@
 from datetime import datetime # importing for current  date and time
+import time
 
 def matchingEngine(cursor,instr_id, trade_symb, ask_price, ask_size, bid_price, bid_size):
 	curr_datetime = datetime.now().strftime('%y-%m-%d %H:%M:%S') # string of current datetime
@@ -6,9 +7,22 @@ def matchingEngine(cursor,instr_id, trade_symb, ask_price, ask_size, bid_price, 
 	curr_date = datetime.now().date() # current date only
 	
 	seq_nbr = getSeqNbr(cursor, trade_symb) # genarating QUOTE_SEQ_NBR
-	
+	# inseting quote
 	insert_fun (cursor, instr_id, trade_symb, ask_price, ask_size, bid_price, bid_size, seq_nbr, curr_datetime, curr_date)
 	
+	time.sleep(60)
+	if bid_price > 0 and bid_size > 0:
+		print "just test"
+
+	# Delete quote when inserted quote is not for bid  
+	else:
+		cursor.execute("DELETE FROM STOCK_QUOTE where INSTRUMENT_ID = %s AND QUOTE_TIME = %s AND BID_PRICE = 0", (instr_id, curr_datetime))
+		print("Inserted Quote has been deleted because bid price and size are 0")
+
+
+
+
+
 
 # return QUOTE_SEQ_NBR
 def getSeqNbr(cursor, trade_symb):
